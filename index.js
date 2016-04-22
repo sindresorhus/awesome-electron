8e2459c28d993b59ccfb99ced8232acbbdf412a6
Zeke Sikelianos <zeke@sikelianos.com>
2016-04-21 21:06:13 -0700

    update tests to support new array format

--- index.js
+++ index.js
@@ -4,48 +4,45 @@ const ignoredCategories = ['table_of_contents', 'tips', 'contribute', 'license']
 const headingPattern = /^## /
 const subHeadingPattern = /^### /
 const itemPattern = /^- \[(.*)\]\((.*)\)(.*)/
-var awesome = []
-var category
-var subcategory
+const slug = function slug(str) { return str.toLowerCase().replace(/ /g, '_')}
 
-const slug = function slug(input) {
-  return input.toLowerCase().replace(/ /g, '_')
+module.exports = function() {
+  var awesome = []
+  var category
+  var subcategory
+
+  fs.readFileSync(path.join(__dirname, 'readme.md'), 'utf8')
+    .split('\n')
+    .forEach(line => {
+
+      if (line.match(headingPattern)) {
+        var cat = slug(line.replace(headingPattern, ''))
+        if (ignoredCategories.indexOf(cat) > -1) return
+        category = cat
+        console.log(category)
+        subcategory = null
+        return
+      }
+
+      if (line.match(subHeadingPattern)) {
+        subcategory = slug(line.replace(subHeadingPattern, ''))
+        return
+      }
+
+      var itemParts = line.match(itemPattern)
+      if (itemParts) {
+        var item = {
+          name: itemParts[1],
+          href: itemParts[2],
+        }
+        if (itemParts[3]) {
+          item.description = itemParts[3].replace(/^ - /, '')
+        }
+        item.category = category
+        if (subcategory) { item.subcategory = subcategory}
+        awesome.push(item)
+      }
+    })
+
+  return awesome
 }
-
-fs.readFileSync(path.join(__dirname, 'readme.md'), 'utf8')
-  .split('\n')
-  .forEach(line => {
-
-    if (line.match(headingPattern)) {
-      var cat = slug(line.replace(headingPattern, ''))
-      if (ignoredCategories.indexOf(cat) > -1) return
-      category = cat
-      console.log(category)
-      subcategory = null
-      return
-    }
-
-    if (line.match(subHeadingPattern)) {
-      subcategory = slug(line.replace(subHeadingPattern, ''))
-      return
-    }
-
-    var itemParts = line.match(itemPattern)
-    if (itemParts) {
-      var item = {
-        name: itemParts[1],
-        href: itemParts[2],
-      }
-      if (itemParts[3]) {
-        item.description = itemParts[3].replace(/^ - /, '')
-      }
-
-      item.category = category
-
-      if (subcategory) { item.subcategory = subcategory}
-
-      awesome.push(item)
-    }
-  })
-
-process.stdout.write(JSON.stringify(awesome, null, 2))
--- test.js
+++ test.js
@@ -1,20 +1,19 @@
 const test = require('tape')
-const awesomeElectron = require('./')
-var sections = ['apps', 'boilerplates', 'tools', 'components']
+const awesome = require('./')()
+var categories = ['apps', 'boilerplates', 'tools', 'components']
 
-test('awesomeElectron', function (t) {
-  t.equal(typeof awesomeElectron, 'object', 'is an object')
-  t.notOk(awesomeElectron.tips, 'does not have tips')
-  t.notOk(awesomeElectron.contribute, 'does not have tips')
-  t.notOk(awesomeElectron.license, 'does not have license')
+test('awesome', function (t) {
+  awesome.forEach(item => {
+    t.ok(item.name.length, `${item.name} has a name`)
+    t.ok(item.href.length, `${item.name} has an href`)
+  })
 
-  sections.forEach(section => {
-    t.ok(Array.isArray(awesomeElectron.apps), `has an array of ${section}`)
+  categories.forEach(category => {
+    var itemsInCategory = awesome.filter(item => item.category === category)
+    t.ok(itemsInCategory.length, `${category} category is not empty`)
 
-    awesomeElectron[section].forEach(item => {
-      t.ok(item.name.length, `${item.name} has a name`)
-      t.ok(item.href.length, `${item.name} has an href`)
-      t.ok(item.description.length, `${item.name} has a description`)
+    itemsInCategory.forEach(item => {
+      t.ok(item.description, `${item.name} has a description`)
     })
   })
 
